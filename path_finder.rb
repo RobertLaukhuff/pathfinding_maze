77c3d11010381608b03e38088644aee2aebd779f
Robert Laukhuff <robert@laukcoder.com>
2019-09-29 22:46:54 -0400

    Added more function declarations and brief descriptions

--- path_finder.rb
+++ path_finder.rb
@@ -1,13 +1,14 @@
 class PathFinder
 
-  attr_reader :maze_array, :maze_start_loc, :maze_end_loc
+  attr_reader :maze_array, :maze_start_loc, :maze_end_loc, :current_position
 
   def initialize(maze_text_file)
     @file = File.new(maze_text_file, "r")
     @maze_array = @file.readlines.map { |line| line.chomp.split("") }
     @maze_start_loc = get_maze_target_loc('S')
     @maze_end_loc = get_maze_target_loc('E')
-    @current_position = @maze_start_loc 
+    @current_position = @maze_start_loc
+    @current_path = []
   end
 
   def get_maze_target_loc(target)
@@ -16,30 +17,35 @@ class PathFinder
     end    
   end
 
-  # def assign_maze_end_loc
-  #   @maze_array.each_with_index do |line, idx|
-  #     return [idx, line.index('E')] if line.index('E')
-  #   end
-  # end
-
-  def current_position
-
+  def check_surroundings
+    # check the contents of the spaces above,
+    # right, bottom, and left of the current_position
+    # TODO: get above position [pos - 1, pos]
+    # TODO: get right position [pos, pos + 1]
+    # TODO: get bottom position [pos + 1, pos]
+    # TODO: get left position [pos, pos - 1]
+    # * be sure adding or subtracting doesn't overflow/underflow the array
+    # * overflow/underflow shouldn't be an issue because there should be
+    # * a wall surrounding the maze but be cautious just in case
   end
 
-  def is_space_open?(current_position)
-
+  def maze_end_found?
+    # check if current_position equals maze_end_loc
+    current_position == @maze_end_loc
   end
 
-  def print_variables
-    p @maze_start
-    p @maze_end
-    p @file
-    p @maze_array
+  def move
+    # move current_position to next space
+    # TODO: add next space to path array
+    # TODO: update current_position to next space    
+  end
+
+  def is_location_open?(location)
+    # check if location is an empty space
+    location == nil
   end
 end
 
 pf = PathFinder.new("./maze.txt")
-# pf.assign_maze_start_loc
-# pf.assign_maze_end_loc
 p pf.maze_start_loc
 p pf.maze_end_loc
